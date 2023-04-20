b69afb4b377d90645d18495a475860bbf80cdbed
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-04-20 20:10:06 -0300

    pp

--- meuAparelho.php
+++ meuAparelho.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <style>
         body{
-            background-color:#a8dadc !important;
+            
+            background-color:#a8dadc ;
 
         }
 
         header{
-             background-color:#1d3557 !important;
+             background-color:#1d3557 ;
         }
     </style>
     <title>Document</title>
@@ -33,10 +34,11 @@
             $cmd =$pdo ->prepare("select id,nomep , data, Tipo_avaria from produto where nome_propretario='$name';");
             $cmd->execute();
     ?>
-    <header>
+    <header >
         <h1>Meu aparelho</h1>
-        <img src="" alt="">
+        
         <p><?php echo $email ?></p>
+        <button id="header" onclick="teste()"></button>
     </header>
 
         <table class="table text-black table-bg">
@@ -65,13 +67,38 @@
         </table>
         <button onclick="teste()" id="button">test</button>
     <script>
+        let img = document.createElement("img")
+         let img1= document.createElement("img")
+        img.src = "./img/lightbulb.svg"
+        img1.src="./img/lightbulb-off.svg"
+        let container =document.querySelector("button")
+        let body = document.querySelector("body")
+        let header= document.querySelector("header")
+        
+        // container.appendChild(img)
         var estado= true;
+         container.appendChild(img);
         function teste(){
             b= document.getElementById('button');
             b.style
             estado = !estado
             console.log(estado)
+            if(estado === true){
+                img1.remove()
+                   header.style.backgroundColor="blue"
+                body.style.backgroundColor="blue"
+                container.appendChild(img);
+                body.style.backgroundColor="white" ;
+            }else{
+                img.remove()
+
+                header.style.backgroundColor="#09030F"
+                body.style.backgroundColor="#070823"
+                body.style.color="white"
+                 container.appendChild(img1);
+            }
         }
+
     </script>
 </body>
 </html>
