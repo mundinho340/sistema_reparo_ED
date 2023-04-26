a953c9e3d283ff0c5faca0195ae1704828aae876
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-04-26 09:06:55 -0300

    Update sistema.php

--- sistema.php
+++ sistema.php
@@ -30,6 +30,7 @@
         
 
     </style>
+    <script src="./controller/script.js"></script>
          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <?php
@@ -125,7 +126,6 @@
             </thead>
             <tbody>
                 <?php
-
                         while($user_data = $cmd->fetch(PDO::FETCH_ASSOC)){
                         echo "<tr>";
                         echo "<td>".$user_data['id']."</td>";
