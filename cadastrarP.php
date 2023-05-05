5760b84fb522d33185601cfa7ea83c415f5f3eda
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-05-05 20:59:20 -0300

    Update cadastrarP.php

--- cadastrarP.php
+++ cadastrarP.php
@@ -8,18 +8,37 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
     <style>
-        form{
-            backgroundlimage:red;
-            border-radius:15px;
-            color:white;
-            margin:auto;
+        header div{
+              display:flex;
+            justify-content:space-between !important;
+            width:1400px;
+        }
+        body{
+            background-color:#a8dadc !important;
+        }
+
+        img{
+            margin-bottom:20px !important;
+        }
+        header{
+            color:#f1faee;
+            display:flex;
+            justify-content: space-between !important;
+            width:100%;
+            background-color:#1d3557 !important;
+            height:90px;
+            align-items:center;
+        }
+        a{
+            text-decoration:none !important;
         }
     </style>
 </head>
 <body>
     <?php
-        session_start();
         require_once('./config.php');
+        session_start();
+      
         if(isset($_POST['submit'])){
             $nome = $_POST['nome'];
             $data = $_POST['data'];
@@ -34,8 +53,48 @@
             $insert -> execute();
             header("location: signin.php");
         }
+          $email=$_GET["email"];
 
     ?>
+     <section class="sidebar" id="sidebar">
+            <article id="header" style="align-items:center;">
+                <div type="hidden" class="user-side" style="align-items:center;"> 
+                 <img src="./img/person-circle.svg" alt="" style="width:40px; height:40px;">
+                    <?php echo $email;?>
+                </div>
+            </article>
+
+            <article id="body">
+                 <nav>
+                     <div>
+                        <img src="./img/card-checklist.svg" alt="">
+                        <a href="listar.php" >listar</a></div>
+                    <br>
+                    <div>
+                        <img src="./img/phone-flip.svg" alt="">
+                        <a href="cadastrarP.php">cadastrar aparelho</a></div>
+                    <br>
+                    <div>
+                        <img src="./img/phone.svg" alt="">
+                        <a href='meuAparelho.php?email=<?php echo $id?>'>visualizar meu aparelho</a></div>
+                    <br>
+                    <div><a href="sair.php"><img src="./img/box-arrow-right.svg" alt="" ">sair</a></div>
+                </nav>
+            </article>
+    </section>
+    <header >
+        <div>
+            <p><?php echo $email ?></p>
+            <button id="button" onclick="teste()"></button>
+        </div>
+         <button style="width:80px; height:40px; align-items:center; text-align:center; border:none; background:transparent;">
+            <div style="margin-left:40px; !important">
+                <img style="width:35px;" src="./img/sliders2.svg" alt="" onclick="sidebar()">
+            </div>
+
+        </button>
+    </header>
+
     <h1>Cadastre o teu equipamento</h1>
      <form action="cadastrarE.php" method="post" id="container" style="width:500px; height:600px; margin:auto; margin-top:10%;">
         <legend>Formulario</legend>
@@ -71,5 +130,6 @@
      </fieldset>
 
      </form>
+     <script src="./side.js"></script>
 </body>
 </html>
